94590bba95002a8b6d0622674d3c4f24e12fd835
Utkarsh <uk4282000@gmail.com>
2025-03-16 15:48:59 +0000

    added manifests for local dev clusters

--- scripts/create-local-dev-cluster.sh
+++ scripts/create-local-dev-cluster.sh
@@ -2,5 +2,5 @@ NAME=$1
 
 FILE_PATH=local-dev/${NAME}-cluster.yaml
 
-cp k3d/cluster-template.yaml $FILE_PATH
+cp capi/cluster-template.yaml $FILE_PATH
 yq --inplace ".metadata.name = \"${NAME}\"" $FILE_PATH
